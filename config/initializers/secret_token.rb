bac598f46459e0c15f7955b1653e57dde2c1b1ec
Yoshihide Chubachi <yoshi@chubachi.net>
2013-12-07 08:20:43 +0000

    Generate a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KikanSystem01::Application.config.secret_key_base = '52fd520b8f86a1ab8ca3cae5ad56a90a8d164e63fa840f65e74489736e5293b37c40ccc4228f10ba0e8e9299ff5f22b554d61100c889a8d56d134b355205b285'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KikanSystem01::Application.config.session_store :cookie_store, key: '_kikan_system_01_session'
