a91ac592b1bf903be251bf04378aa06f9fdc990e
vagrant <pyar6329@gmail.com>
2013-12-21 09:54:30 +0000

    second

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131221062348) do
+ActiveRecord::Schema.define(version: 20131221094300) do
 
   create_table "students", force: true do |t|
     t.string   "student_num"
